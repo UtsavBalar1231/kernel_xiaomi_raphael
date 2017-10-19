673d614e8a950874488977ebce154fcebed1548e
Jeff Vanhoof <qjv001@motorola.com>
2017-10-19 05:39:30 -0500

    lowmemorykiller: Account for extra_free_kbytes in calcuations
    Account for extra_free_kbytes in calcuations
    
    CRs-fixed: (CR)
    Signed-off-by: Jeff Vanhoof <qjv001@motorola.com>
    Change-Id: I2c44a1775d43fad40cf49d6303b7552c2c089b6e
    Reviewed-on: https://gerrit.mot.com/1076213
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -47,6 +47,8 @@
 #include "trace/lowmemorykiller.h"
 
 static u32 lowmem_debug_level = 1;
+extern int extra_free_kbytes;
+
 static short lowmem_adj[6] = {
 	0,
 	1,
@@ -117,7 +119,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		minfree = lowmem_minfree[i];
+		minfree = lowmem_minfree[i] +
+			  ((extra_free_kbytes * 1024) / PAGE_SIZE);
 		if (other_free < minfree && other_file < minfree) {
 			min_score_adj = lowmem_adj[i];
 			minfree_count_offset = i;
