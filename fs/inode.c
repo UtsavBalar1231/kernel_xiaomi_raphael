6d5a4a7b1853f0b8d2a1654bfa97d4a3aa4e398a
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-20 00:57:29 +0900

    time: move frequently used functions to headers and declare them inline
    Those function are frequently used in various places and declaring them inline
    can reduce overheads.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- fs/inode.c
+++ fs/inode.c
@@ -2123,29 +2123,6 @@ void inode_nohighmem(struct inode *inode)
 }
 EXPORT_SYMBOL(inode_nohighmem);
 
-/**
- * current_time - Return FS time
- * @inode: inode.
- *
- * Return the current time truncated to the time granularity supported by
- * the fs.
- *
- * Note that inode and inode->sb cannot be NULL.
- * Otherwise, the function warns and returns time without truncation.
- */
-struct timespec current_time(struct inode *inode)
-{
-	struct timespec now = current_kernel_time();
-
-	if (unlikely(!inode->i_sb)) {
-		WARN(1, "current_time() called with uninitialized super_block in the inode");
-		return now;
-	}
-
-	return timespec_trunc(now, inode->i_sb->s_time_gran);
-}
-EXPORT_SYMBOL(current_time);
-
 /*
  * Generic function to check FS_IOC_SETFLAGS values and reject any invalid
  * configurations.
