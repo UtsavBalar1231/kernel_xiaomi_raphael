ac698ef725964002cfa6c23f07c3d8b03de8f5be
Waiman Long <longman@redhat.com>
2018-03-30 17:27:58 -0400

    locking/rwsem: Add DEBUG_RWSEMS to look for lock/unlock mismatches
    For a rwsem, locking can either be exclusive or shared. The corresponding
    exclusive or shared unlock must be used. Otherwise, the protected data
    structures may get corrupted or the lock may be in an inconsistent state.
    
    In order to detect such anomaly, a new configuration option DEBUG_RWSEMS
    is added which can be enabled to look for such mismatches and print
    warnings that that happens.
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Acked-by: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1522445280-7767-2-git-send-email-longman@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/locking/rwsem.h
+++ kernel/locking/rwsem.h
@@ -31,6 +31,12 @@ struct rwsem_waiter {
 	enum rwsem_waiter_type type;
 };
 
+#ifdef CONFIG_DEBUG_RWSEMS
+# define DEBUG_RWSEMS_WARN_ON(c)	DEBUG_LOCKS_WARN_ON(c)
+#else
+# define DEBUG_RWSEMS_WARN_ON(c)
+#endif
+
 #ifdef CONFIG_RWSEM_SPIN_ON_OWNER
 /*
  * All writes to owner are protected by WRITE_ONCE() to make sure that
@@ -56,7 +62,7 @@ static inline void rwsem_set_reader_owned(struct rw_semaphore *sem)
 	 * do a write to the rwsem cacheline when it is really necessary
 	 * to minimize cacheline contention.
 	 */
-	if (sem->owner != RWSEM_READER_OWNED)
+	if (READ_ONCE(sem->owner) != RWSEM_READER_OWNED)
 		WRITE_ONCE(sem->owner, RWSEM_READER_OWNED);
 }
 
