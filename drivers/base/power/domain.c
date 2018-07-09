cec1d341e0e580c620e917cb04369cd7d3f81eb7
Rob Herring <robh@kernel.org>
2018-07-09 09:41:53 -0600

    PM / Domains: Stop deferring probe at the end of initcall
    All PM domain drivers must be built-in (at least those using DT), so
    there is no point deferring probe after initcalls are done. Continuing
    to defer probe may prevent booting successfully even if managing PM
    domains is not required. This can happen if the user failed to enable
    the driver or if power-domains are added to a platform's DT, but there
    is not yet a driver (e.g. a new DTB with an old kernel).
    
    Call the driver core function driver_deferred_probe_check_init_done()
    instead of just returning -EPROBE_DEFER to stop deferring probe when
    initcalls are done.
    
    Acked-by: "Rafael J. Wysocki" <rjw@rjwysocki.net>
    Cc: Kevin Hilman <khilman@kernel.org>
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Cc: Pavel Machek <pavel@ucw.cz>
    Cc: Len Brown <len.brown@intel.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: linux-pm@vger.kernel.org
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -2140,7 +2140,7 @@ int genpd_dev_pm_attach(struct device *dev)
 		mutex_unlock(&gpd_list_lock);
 		dev_dbg(dev, "%s() failed to find PM domain: %ld\n",
 			__func__, PTR_ERR(pd));
-		return -EPROBE_DEFER;
+		return driver_deferred_probe_check_state(dev);
 	}
 
 	dev_dbg(dev, "adding to PM domain %s\n", pd->name);
