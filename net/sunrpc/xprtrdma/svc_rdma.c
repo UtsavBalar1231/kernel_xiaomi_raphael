6e4e15a90d548b82812bbc45d595a75bac388a75
YueHaibing <yuehaibing@huawei.com>
2018-08-01 14:28:10 +0800

    sunrpc: remove redundant variables 'checksumlen','blocksize' and 'data'
    Variables 'checksumlen','blocksize' and 'data' are being assigned,
    but are never used, hence they are redundant and can be removed.
    
    Fix the following warning:
    
      net/sunrpc/auth_gss/gss_krb5_wrap.c:443:7: warning: variable ‘blocksize’ set but not used [-Wunused-but-set-variable]
      net/sunrpc/auth_gss/gss_krb5_crypto.c:376:15: warning: variable ‘checksumlen’ set but not used [-Wunused-but-set-variable]
      net/sunrpc/xprtrdma/svc_rdma.c:97:9: warning: variable ‘data’ set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- net/sunrpc/xprtrdma/svc_rdma.c
+++ net/sunrpc/xprtrdma/svc_rdma.c
@@ -93,7 +93,6 @@ static int read_reset_stat(struct ctl_table *table, int write,
 		atomic_set(stat, 0);
 	else {
 		char str_buf[32];
-		char *data;
 		int len = snprintf(str_buf, 32, "%d\n", atomic_read(stat));
 		if (len >= 32)
 			return -EFAULT;
@@ -102,7 +101,6 @@ static int read_reset_stat(struct ctl_table *table, int write,
 			*lenp = 0;
 			return 0;
 		}
-		data = &str_buf[*ppos];
 		len -= *ppos;
 		if (len > *lenp)
 			len = *lenp;
