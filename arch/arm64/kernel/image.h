292b5835326af2c72f486d18456625ba60b856a1
Nick Desaulniers <ndesaulniers@google.com>
2019-01-08 15:14:30 -0800

    ANDROID: arm64: add support for building the KASLR kernel with LLVM lld
    Move the definition of some __efistub_ decorated section markers to the
    linker script, which permits us to assign them as relative quantities
    (since using an intermediate assignment loses the section relative
    property when using ld.lld)
    
    This was split off of the patch originally written by Ard Biesheuvel.
    It's not clear if this is better fixed by support for forward section
    declarations in LLD, or reordering of the sections as we've done here.
    
    Bug: 63740206
    Bug: 120083959
    Change-Id: I9bd77de9bac7c970e6082e90302a6eacd82a7ce7
    Link: https://www.spinics.net/lists/arm-kernel/msg691488.html
    Link: https://github.com/ClangBuiltLinux/linux/issues/280
    Sugguested-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- arch/arm64/kernel/image.h
+++ arch/arm64/kernel/image.h
@@ -76,12 +76,6 @@
 __efistub_stext_offset = stext - _text;
 
 /*
- * Use ABSOLUTE() to avoid ld.lld treating this as a relative symbol:
- * https://github.com/ClangBuiltLinux/linux/issues/561
- */
-__efistub_stext_offset = ABSOLUTE(stext - _text);
-
-/*
  * The EFI stub has its own symbol namespace prefixed by __efistub_, to
  * isolate it from the kernel proper. The following symbols are legally
  * accessed by the stub, so provide some aliases to make them accessible.
@@ -107,9 +101,6 @@ __efistub___memmove		= __pi_memmove;
 __efistub___memset		= __pi_memset;
 #endif
 
-__efistub__text			= _text;
-__efistub__end			= _end;
-__efistub__edata		= _edata;
 __efistub_screen_info		= screen_info;
 
 #endif
