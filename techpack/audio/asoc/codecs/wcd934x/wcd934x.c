ecac3863932314d3ad47430535c0bddc7916dbe6
freak07 <michalechner92@googlemail.com>
2019-02-20 10:22:43 +0100

    techpack: use power efficient workingqueues
    (cherry picked from commit 75ed04dc1f7e7da565e41b7190fb44f078c5b4d6)
    (cherry picked from commit a23fc1d9e2a6166dde561c6e5cf8f1e09e0a5c0c)
    (cherry picked from commit efed5ad0c48e83e66b4be7f33147aa192e47e498)

--- techpack/audio/asoc/codecs/wcd934x/wcd934x.c
+++ techpack/audio/asoc/codecs/wcd934x/wcd934x.c
@@ -2129,7 +2129,7 @@ static int tavil_codec_enable_spkr_anc(struct snd_soc_dapm_widget *w,
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
 		ret = tavil_codec_enable_anc(w, kcontrol, event);
-		schedule_delayed_work(&tavil->spk_anc_dwork.dwork,
+		queue_delayed_work(system_power_efficient_wq, &tavil->spk_anc_dwork.dwork,
 				      msecs_to_jiffies(spk_anc_en_delay));
 		break;
 	case SND_SOC_DAPM_POST_PMD:
@@ -4510,11 +4510,11 @@ static int tavil_codec_enable_dec(struct snd_soc_dapm_widget *w,
 		usleep_range(1000, 1010);
 		snd_soc_update_bits(codec, hpf_gate_reg, 0x02, 0x00);
 		/* schedule work queue to Remove Mute */
-		schedule_delayed_work(&tavil->tx_mute_dwork[decimator].dwork,
+		queue_delayed_work(system_power_efficient_wq, &tavil->tx_mute_dwork[decimator].dwork,
 				      msecs_to_jiffies(tx_unmute_delay));
 		if (tavil->tx_hpf_work[decimator].hpf_cut_off_freq !=
 							CF_MIN_3DB_150HZ)
-			schedule_delayed_work(
+			queue_delayed_work(system_power_efficient_wq,
 					&tavil->tx_hpf_work[decimator].dwork,
 					msecs_to_jiffies(300));
 		/* apply gain after decimator is enabled */
@@ -9173,7 +9173,7 @@ static int tavil_dig_core_power_collapse(struct tavil_priv *tavil,
 
 	if (req_state == POWER_COLLAPSE) {
 		if (tavil->power_active_ref == 0) {
-			schedule_delayed_work(&tavil->power_gate_work,
+			queue_delayed_work(system_power_efficient_wq, &tavil->power_gate_work,
 			msecs_to_jiffies(dig_core_collapse_timer * 1000));
 		}
 	} else if (req_state == POWER_RESUME) {
