040b3ba47482c5974af1609a14b51db004a86d1b
Minchan Kim <minchan@google.com>
2019-03-11 11:42:01 +0900

    Revert "mm: protect VMA modifications using VMA sequence count"
    This reverts commit 41530993596b811ce3349224ed6f35839678b071.
    
    Bug: 128240262
    Change-Id: If31a4c81badd891e6ca5740dbd022b5edbe47254
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1028,7 +1028,6 @@ static void collapse_huge_page(struct mm_struct *mm,
 	if (mm_find_pmd(mm, address) != pmd)
 		goto out;
 
-	vm_write_begin(vma);
 	anon_vma_lock_write(vma->anon_vma);
 
 	pte = pte_offset_map(pmd, address);
@@ -1064,7 +1063,6 @@ static void collapse_huge_page(struct mm_struct *mm,
 		pmd_populate(mm, pmd, pmd_pgtable(_pmd));
 		spin_unlock(pmd_ptl);
 		anon_vma_unlock_write(vma->anon_vma);
-		vm_write_end(vma);
 		result = SCAN_FAIL;
 		goto out;
 	}
@@ -1099,7 +1097,6 @@ static void collapse_huge_page(struct mm_struct *mm,
 	set_pmd_at(mm, address, pmd, _pmd);
 	update_mmu_cache_pmd(vma, address, pmd);
 	spin_unlock(pmd_ptl);
-	vm_write_end(vma);
 
 	*hpage = NULL;
 
