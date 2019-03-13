f37b9f4a25e38fa7727f491365e9dbc095c08f7a
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-13 13:31:58 +0900

    msm_geni_serial: guard PM related logs with #ifdef DEBUG_PM
    This is useful but spams the kmsg too much
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -31,6 +31,9 @@
 #include <soc/qcom/boot_stats.h>
 #include <linux/dma-mapping.h>
 
+/* Show PM related logs */
+//#define DEBUG_PM
+
 /* UART specific GENI registers */
 #define SE_UART_LOOPBACK_CFG		(0x22C)
 #define SE_UART_TX_TRANS_CFG		(0x25C)
@@ -320,10 +323,13 @@ static int vote_clock_on(struct uart_port *uport)
 		return ret;
 	}
 	port->ioctl_count++;
+
+#ifdef DEBUG_PM
 	dev_info(uport->dev,
 		"pid: %d, comm: %s voted clock to be on, "
 		"ioctl_count: %d\n",
 			current->pid, current->comm, port->ioctl_count);
+#endif
 
 	return 0;
 }
@@ -348,10 +354,13 @@ static int vote_clock_off(struct uart_port *uport)
 				__func__, port->ioctl_count);
 		port->ioctl_count = 0;
 	}
+
+#ifdef DEBUG_PM
 	dev_info(uport->dev,
 		"pid: %d, comm: %s voted clock to be off, "
 		"ioctl_count: %d\n",
 			current->pid, current->comm, port->ioctl_count);
+#endif
 	msm_geni_serial_power_off(uport);
 
 	return 0;
@@ -1548,8 +1557,10 @@ static irqreturn_t msm_geni_wakeup_isr(int isr, void *dev)
 		port->edge_count = 0;
 		tty_flip_buffer_push(tty->port);
 		__pm_wakeup_event(&port->geni_wake, WAKEBYTE_TIMEOUT_MSEC);
+#ifdef DEBUG_PM
 		dev_info(uport->dev, "%s: Holding wakelock for %d ms\n",
 					__func__, WAKEBYTE_TIMEOUT_MSEC);
+#endif
 	} else if (port->edge_count < 2) {
 		port->edge_count++;
 	}
