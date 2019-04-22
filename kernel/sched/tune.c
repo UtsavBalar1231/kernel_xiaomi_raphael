0820cb6c24b299598e393f200468d1255ff0b46b
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-22 11:22:41 +0530

    sched/tune: Hard-code top-app's stune boost to 1
    Hard-code top-app's stune boost to 1 so that top-app processes are still
    preferred to run on big cluster CPUs without significantly affecting the
    CPU governor's frequency selection.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -630,6 +630,9 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 {
 	struct schedtune *st = css_st(css);
 
+	if (!strcmp(css->cgroup->kn->name, "top-app"))
+		boost = 1;
+
 	if (boost < -100 || boost > 100)
 		return -EINVAL;
 
