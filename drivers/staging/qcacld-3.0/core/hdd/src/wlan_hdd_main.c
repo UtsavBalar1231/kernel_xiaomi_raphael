eaf4fdd39d02ad91831067604acbbbbc2295a0cc
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-05 14:54:15 -0700

    qcacld-3.0: Nuke as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [kdrag0n: adapted for Pixel 3 Wi-Fi stack]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    
    Adapted for qcacld 5.2.03
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -8483,7 +8483,6 @@ void wlan_hdd_deinit_tx_rx_histogram(struct hdd_context *hdd_ctx)
 	hdd_ctx->hdd_txrx_hist = NULL;
 }
 
-#ifdef WLAN_DEBUG
 static uint8_t *convert_level_to_string(uint32_t level)
 {
 	switch (level) {
@@ -8500,8 +8499,6 @@ static uint8_t *convert_level_to_string(uint32_t level)
 		return "INVAL";
 	}
 }
-#endif
-
 
 /**
  * wlan_hdd_display_tx_rx_histogram() - display tx rx histogram
