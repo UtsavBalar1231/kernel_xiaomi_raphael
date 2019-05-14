89a984290bf9a26be802bc55347a9453b08305f3
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-05-14 15:42:25 -0700

    compiler: allow all arches to enable CONFIG_OPTIMIZE_INLINING
    Commit 60a3cdd06394 ("x86: add optimized inlining") introduced
    CONFIG_OPTIMIZE_INLINING, but it has been available only for x86.
    
    The idea is obviously arch-agnostic.  This commit moves the config entry
    from arch/x86/Kconfig.debug to lib/Kconfig.debug so that all
    architectures can benefit from it.
    
    This can make a huge difference in kernel image size especially when
    CONFIG_OPTIMIZE_FOR_SIZE is enabled.
    
    For example, I got 3.5% smaller arm64 kernel for v5.1-rc1.
    
      dec       file
      18983424  arch/arm64/boot/Image.before
      18321920  arch/arm64/boot/Image.after
    
    This also slightly improves the "Kernel hacking" Kconfig menu as
    e61aca5158a8 ("Merge branch 'kconfig-diet' from Dave Hansen') suggested;
    this config option would be a good fit in the "compiler option" menu.
    
    Link: http://lkml.kernel.org/r/20190423034959.13525-12-yamada.masahiro@socionext.com
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Borislav Petkov <bp@suse.de>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Boris Brezillon <bbrezillon@kernel.org>
    Cc: Brian Norris <computersforpeace@gmail.com>
    Cc: Christophe Leroy <christophe.leroy@c-s.fr>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Marek Vasut <marek.vasut@gmail.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Malaterre <malat@debian.org>
    Cc: Miquel Raynal <miquel.raynal@bootlin.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Cc: Stefan Agner <stefan@agner.ch>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [kdrag0n: Backported to k4.14]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -90,8 +90,7 @@
  * of extern inline functions at link time.
  * A lot of inline functions can cause havoc with function tracing.
  */
-#if !defined(CONFIG_ARCH_SUPPORTS_OPTIMIZED_INLINING) ||		\
-    !defined(CONFIG_OPTIMIZE_INLINING) || (__GNUC__ < 4)
+#if !defined(CONFIG_OPTIMIZE_INLINING) || (__GNUC__ < 4)
 #define inline \
 	inline __attribute__((always_inline, unused)) notrace __gnu_inline
 #else
