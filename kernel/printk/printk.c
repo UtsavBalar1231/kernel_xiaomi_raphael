467179f1c9a447297a5f2502b17ea03bb769bf63
Yaroslav Furman <yaro330@gmail.com>
2019-06-06 17:17:20 +0300

    printk: block healthd messages in kernel log
    Initial idea from here but optimized to prevent overhead.
    https://github.com/kdrag0n/velocity_dream/commit/2dac07a943ba6aef3412db980c3dba30bf427f0c
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -813,10 +813,18 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			/* QG-D */
+			if (line[0] == 'h') {
+				for (u = 0; u < 10; ++u) {
+					if (line[u] == 'd')
+						goto free;
+				}
+			}
 		}
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
+free:
 	kfree(buf);
 	return ret;
 }
