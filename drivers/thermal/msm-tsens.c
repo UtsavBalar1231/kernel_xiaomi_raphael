07d0b16fd4765ab40e8589785f4a865c85dbbd2c
Yaroslav Furman <yaro330@gmail.com>
2019-06-22 00:59:56 +0300

    msm-tsens: ifdef IPC logging related code
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/thermal/msm-tsens.c
+++ drivers/thermal/msm-tsens.c
@@ -234,7 +234,9 @@ int tsens_tm_probe(struct platform_device *pdev)
 {
 	struct tsens_device *tmdev = NULL;
 	int rc;
+#ifdef CONFIG_IPC_LOGGING
 	char tsens_name[40];
+#endif
 
 	if (!(pdev->dev.of_node))
 		return -ENODEV;
@@ -271,6 +273,7 @@ int tsens_tm_probe(struct platform_device *pdev)
 		return rc;
 	}
 
+#ifdef CONFIG_IPC_LOGGING
 	snprintf(tsens_name, sizeof(tsens_name), "tsens_%pa_0",
 					&tmdev->phys_addr_tm);
 
@@ -297,6 +300,7 @@ int tsens_tm_probe(struct platform_device *pdev)
 	if (!tmdev->ipc_log2)
 		pr_err("%s : unable to create IPC Logging 2 for tsens %pa",
 					__func__, &tmdev->phys_addr_tm);
+#endif
 
 	list_add_tail(&tmdev->list, &tsens_device_list);
 	platform_set_drvdata(pdev, tmdev);
