76a5931290a31226b2bf1ba0ee4098fa3fc9e5d0
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-02 01:54:39 -0700

    xattr: Avoid dynamically allocating memory in getxattr
    Although the maximum xattr size is too big to fit on the stack (64 KiB),
    we can still fulfill most getxattr requests with a 4 KiB stack
    allocation, thereby improving performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- fs/xattr.c
+++ fs/xattr.c
@@ -531,6 +531,7 @@ getxattr(struct dentry *d, const char __user *name, void __user *value,
 	ssize_t error;
 	void *kvalue = NULL;
 	char kname[XATTR_NAME_MAX + 1];
+	char kvalue_onstack[SZ_4K] __aligned(8);
 
 	error = strncpy_from_user(kname, name, sizeof(kname));
 	if (error == 0 || error == sizeof(kname))
@@ -539,11 +540,16 @@ getxattr(struct dentry *d, const char __user *name, void __user *value,
 		return error;
 
 	if (size) {
-		if (size > XATTR_SIZE_MAX)
-			size = XATTR_SIZE_MAX;
-		kvalue = kvzalloc(size, GFP_KERNEL);
-		if (!kvalue)
-			return -ENOMEM;
+		if (size <= ARRAY_SIZE(kvalue_onstack)) {
+			kvalue = kvalue_onstack;
+			memset(kvalue, 0, size);
+		} else {
+			if (size > XATTR_SIZE_MAX)
+				size = XATTR_SIZE_MAX;
+			kvalue = kvzalloc(size, GFP_KERNEL);
+			if (!kvalue)
+				return -ENOMEM;
+		}
 	}
 
 	error = vfs_getxattr(d, kname, kvalue, size);
@@ -559,7 +565,8 @@ getxattr(struct dentry *d, const char __user *name, void __user *value,
 		error = -E2BIG;
 	}
 
-	kvfree(kvalue);
+	if (kvalue != kvalue_onstack)
+		kvfree(kvalue);
 
 	return error;
 }
