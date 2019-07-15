54e01f01864fd70ee7bd7155ce34057e31997dc2
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-16 03:42:47 +0900

    Revert "ARM: gic: Add support for logging interrupts in RTB"
    This reverts commit b6f137ab06e63f2323d102c562a7381000a76bd2.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -41,7 +41,6 @@
 #include <linux/irqchip.h>
 #include <linux/irqchip/chained_irq.h>
 #include <linux/irqchip/arm-gic.h>
-#include <linux/msm_rtb.h>
 #ifdef CONFIG_PM
 #include <linux/syscore_ops.h>
 #endif
@@ -514,7 +513,6 @@ static void __exception_irq_entry gic_handle_irq(struct pt_regs *regs)
 				writel_relaxed(irqstat, cpu_base + GIC_CPU_EOI);
 			isb();
 			handle_domain_irq(gic->domain, irqnr, regs);
-			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 			continue;
 		}
 		if (irqnr < 16) {
@@ -532,7 +530,6 @@ static void __exception_irq_entry gic_handle_irq(struct pt_regs *regs)
 			smp_rmb();
 			handle_IPI(irqnr, regs);
 #endif
-			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 			continue;
 		}
 		break;
