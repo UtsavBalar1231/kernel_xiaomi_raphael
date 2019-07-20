bdf12c7f28010f126666e8113def0bccdca80330
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-20 10:06:29 -0700

    msm: kgsl: Wake GPU upon receiving an ioctl rather than upon touch input
    Waking the GPU upon touch wastes power when the screen is being touched
    in a way that does not induce animation or any actual need for GPU usage.
    Instead of preemptively waking the GPU on touch input, wake it up upon
    receiving a IOCTL_KGSL_GPU_COMMAND ioctl since it is a sign that the GPU
    will soon be needed.
    
    Change-Id: I4133bbeacefe832c4c1f5178b54ba94309ca7bfe
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -68,8 +68,7 @@ enum kgsl_event_results {
 	KGSL_EVENT_CANCELLED = 2,
 };
 
-#define KGSL_FLAG_WAKE_ON_TOUCH BIT(0)
-#define KGSL_FLAG_SPARSE        BIT(1)
+#define KGSL_FLAG_SPARSE        BIT(0)
 
 /*
  * "list" of event types for ftrace symbolic magic
