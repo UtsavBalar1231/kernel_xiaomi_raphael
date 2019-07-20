ad7f492b296ddb0747869bab8db7e74b0ab24640
flar2 <asegaert@gmail.com>
2019-07-19 20:29:49 -0400

    kcal/klapse: remove min limit
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -233,7 +233,7 @@ void sde_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	struct drm_msm_pcc *pcc_cfg;
 	struct drm_msm_pcc_coeff *coeffs = NULL;
 	int i = 0;
-	int kcal_min = 20;
+	int kcal_min = 0;
 	u32 base = 0;
 	u32 opcode = 0, local_opcode = 0;
 
