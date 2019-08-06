8bd2125f09f930f114d1c1b5d838a865fb82586e
Manjunathappa Prakash <prakashpm@codeaurora.org>
2019-08-06 16:47:08 -0700

    qcacld-3.0: stop bus bandwidth timer during runtime suspend
    During runtime suspend there is no need to run the periodic bus
    bandwidth timer as there is no data traffic.
    
    Change-Id: I7b53b773e5f9fa2c803e8f4dbb186a30d1dc696b
    CRs-Fixed: 2504215

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -7488,7 +7488,6 @@ void hdd_wlan_exit(struct hdd_context *hdd_ctx)
 
 	hdd_wlan_stop_modules(hdd_ctx, false);
 
-	hdd_bus_bw_compute_timer_stop(hdd_ctx);
 	hdd_bus_bandwidth_deinit(hdd_ctx);
 	hdd_driver_memdump_deinit();
 
@@ -11524,6 +11523,7 @@ int hdd_wlan_stop_modules(struct hdd_context *hdd_ctx, bool ftm_mode)
 			hdd_psoc_idle_timer_start(hdd_ctx);
 			cds_set_module_stop_in_progress(false);
 
+			hdd_bus_bw_compute_timer_stop(hdd_ctx);
 			return 0;
 		}
 	}
@@ -11671,6 +11671,7 @@ int hdd_wlan_stop_modules(struct hdd_context *hdd_ctx, bool ftm_mode)
 	hdd_info("Wlan transitioned (now CLOSED)");
 
 	pld_request_bus_bandwidth(hdd_ctx->parent_dev, PLD_BUS_WIDTH_NONE);
+	hdd_bus_bw_compute_timer_stop(hdd_ctx);
 
 done:
 	cds_set_module_stop_in_progress(false);
