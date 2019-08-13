1a165cca48c67ccfb1242c2b34ae62d2acbe8976
Alan Chen <alache@codeaurora.org>
2019-08-12 17:08:22 -0700

    qcacld-3.0: Clear recovery flag after idle restart
    Clear recovery flag after idle restart such that next time wifi can
    turn on properly.
    
    Change-Id: I7063a9edd08334a84068f88c0564dd4f2aabf666
    CRs-Fixed: 2507899

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -9438,6 +9438,7 @@ static int __hdd_mode_change_psoc_idle_shutdown(struct hdd_context *hdd_ctx)
 
 int hdd_psoc_idle_shutdown(struct device *dev)
 {
+	int ret;
 	struct hdd_context *hdd_ctx = cds_get_context(QDF_MODULE_ID_HDD);
 
 	if (!hdd_ctx) {
@@ -9446,9 +9447,13 @@ int hdd_psoc_idle_shutdown(struct device *dev)
 	}
 
 	if (is_mode_change_psoc_idle_shutdown)
-		return __hdd_mode_change_psoc_idle_shutdown(hdd_ctx);
+		ret = __hdd_mode_change_psoc_idle_shutdown(hdd_ctx);
 	else
-		return __hdd_psoc_idle_shutdown(hdd_ctx);
+		ret =  __hdd_psoc_idle_shutdown(hdd_ctx);
+
+	cds_set_recovery_in_progress(false);
+
+	return ret;
 }
 
 static int __hdd_psoc_idle_restart(struct hdd_context *hdd_ctx)
