04fe1d17d267ff31953a46392beb26955102451d
Sravan Kumar Kairam <sgoud@codeaurora.org>
2019-08-21 20:59:51 +0530

    qcacmn: Add runtime pm apis to record last dp rx busy mark
    Add runtime pm apis to record last busy mark set by data path
    during dp rx process.
    
    Change-Id: I694ef6ddec8f11fed44bf3b9e5ae18ad93b6ec24
    CRs-Fixed: 2512326

--- dp/wifi3.0/dp_rx.c
+++ dp/wifi3.0/dp_rx.c
@@ -1708,7 +1708,7 @@ uint32_t dp_rx_process(struct dp_intr *int_ctx, void *hal_ring,
 	qdf_assert_always(hal_soc);
 
 	scn = soc->hif_handle;
-	hif_pm_runtime_mark_last_busy(scn);
+	hif_pm_runtime_mark_dp_rx_busy(scn);
 	intr_id = int_ctx->dp_intr_id;
 
 more_data:
