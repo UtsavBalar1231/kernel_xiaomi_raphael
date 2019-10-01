bef6f1cfe4fbb190ef1c0ce08184875297f0c3d8
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-01 14:40:51 +0900

    Revert "ext4: wire up new fscrypt ioctls"
    This reverts commit 78aa6228c365d71a5bdf54201fe4d1f631ce53de.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1051,9 +1051,6 @@ static int ext4_drop_inode(struct inode *inode)
 {
 	int drop = generic_drop_inode(inode);
 
-	if (!drop)
-		drop = fscrypt_drop_inode(inode);
-
 	trace_ext4_drop_inode(inode, drop);
 	return drop;
 }
