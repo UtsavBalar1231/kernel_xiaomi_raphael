fbb5a217381502bdaf4be5cd6f0c8ba1f37fc7a2
GuaiYiHu <guaiyihu@foxmail.com>
2019-11-12 17:47:44 +0800

    ASoC: Avoid spk_id_get duplicate defination
    Change-Id: I03e0e05434d3998e921acdfc1b36d2916a3dcd67
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- asoc/codecs/tas2557/tas2557-core.h
+++ asoc/codecs/tas2557/tas2557-core.h
@@ -76,6 +76,6 @@ int tas2557_parse_dt(struct device *dev, struct tas2557_priv *pTAS2557);
 int tas2557_get_DAC_gain(struct tas2557_priv *pTAS2557, unsigned char *pnGain);
 int tas2557_set_DAC_gain(struct tas2557_priv *pTAS2557, unsigned int nGain);
 int tas2557_configIRQ(struct tas2557_priv *pTAS2557);
-int spk_id_get(struct device_node *np);
+int tas2557_spk_id_get(struct device_node *np);
 
 #endif /* _TAS2557_CORE_H */
