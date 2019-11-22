b8be7d9b8b4f516cbc13b67a050ee598c6ed75b0
Naitik Bharadiya <bharad@codeaurora.org>
2019-11-22 14:41:00 +0530

    ARM: SDM: bringup changes for SDM660
    Bring up changes for SDM660 on 4.14 kernel.
    
    Change-Id: I338a4a19eee9fb8757c5ef49c307ec794610fc11
    Signed-off-by: Naitik Bharadiya <bharad@codeaurora.org>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -105,6 +105,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,atoll")
 #define early_machine_is_atollp()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,atollp")
+#define early_machine_is_sdm660()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdm660")
 #else
 #define of_board_is_sim()		0
 #define of_board_is_rumi()		0
@@ -147,6 +149,7 @@
 #define early_machine_is_qcs410()       0
 #define early_machine_is_atoll()	0
 #define early_machine_is_atollp()	0
+#define early_machine_is_sdm660()	0
 #endif
 
 #define PLATFORM_SUBTYPE_MDM	1
@@ -191,6 +194,7 @@ enum msm_cpu {
 	MSM_CPU_QCS410,
 	MSM_CPU_ATOLL,
 	MSM_CPU_ATOLLP,
+	MSM_CPU_SDM660,
 };
 
 struct msm_soc_info {
