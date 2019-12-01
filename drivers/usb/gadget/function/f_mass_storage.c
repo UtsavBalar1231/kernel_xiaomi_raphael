1ed089e1e8c48f5e8a85d81e3ba8922d97858c4d
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-01 16:05:50 +0000

    drivers: usb: apply xiaomi power supply changes
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -2290,11 +2290,11 @@ static int fsg_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 {
 	struct fsg_dev *fsg = fsg_from_func(f);
 	fsg->common->new_fsg = fsg;
+	raise_exception(fsg->common, FSG_STATE_CONFIG_CHANGE);
 
 	/* prevents usb LPM until thread runs to completion */
 	usb_gadget_autopm_get_async(fsg->common->gadget);
 
-	raise_exception(fsg->common, FSG_STATE_CONFIG_CHANGE);
 	return USB_GADGET_DELAYED_STATUS;
 }
 
