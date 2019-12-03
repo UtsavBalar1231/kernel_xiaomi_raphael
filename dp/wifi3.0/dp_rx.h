7fd92120ae4c4741360ca5f1ca6b62ff7ca9a231
Mohit Khanna <mkhannaqca@codeaurora.org>
2019-12-03 11:54:58 -0800

    qcacmn: Use HAL_RX_BUF_RBM_SW3_RBM for defrag pkts
    Currently defragmented packets use HAL_RX_BUF_RBM_SW1_RBM as the RBM
    value for the defragmented packets which are re-injected into REO.
    Thus, if REO encounters any error while handling these packets, they
    would end up in WBM2SW1 ring (via WBM), which is managed by the FW. The
    FW will eventually recycle these buffers back to RXDMA via its refill
    process. As a part of defragmentation, host does a 802.11 -> 802.3
    header conversion. This is resulting in an address which is not 4
    byte aligned. Hence, when RXDMA tries to use these addresses (after FW
    recycles them), it may lead to issues.
    
    Change the RBM value of the defragmented buffers which are
    re-injected. Now, if REO ends up throwing an error for these
    packets, they wll end up in WBM2SW3, which is managed by the host.
    The host can then drop these packets and replenish RXDMA with 4 byte
    aligned buffers (via FW).
    
    Change-Id: I9d9c25385978d5be855699feb28d292c6f3fffdd
    CRs-Fixed: 2572483

--- dp/wifi3.0/dp_rx.h
+++ dp/wifi3.0/dp_rx.h
@@ -35,6 +35,8 @@
 #endif /* RXDMA_OPTIMIZATION */
 
 #ifdef QCA_HOST2FW_RXBUF_RING
+/* RBM value used for re-injecting defragmented packets into REO */
+#define DP_DEFRAG_RBM HAL_RX_BUF_RBM_SW3_BM
 #define DP_WBM2SW_RBM HAL_RX_BUF_RBM_SW1_BM
 
 /**
@@ -43,6 +45,7 @@
  */
 #define DP_RX_DESC_ALLOC_MULTIPLIER 1
 #else
+#define DP_DEFRAG_RBM DP_WBM2SW_RBM
 #define DP_WBM2SW_RBM HAL_RX_BUF_RBM_SW3_BM
 
 /**
