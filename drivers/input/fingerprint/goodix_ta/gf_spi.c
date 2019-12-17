354d6d07f40f9161392a9fdc13655419a8f4f1b1
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-17 04:47:50 +0000

    fingerprint: goodix_ta: use qcom drm notifier for event blank calls
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -745,7 +745,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 	unsigned int blank;
 	char temp[4] = { 0x0 };
 
-	if (val != DRM_EVENT_BLANK) {
+	if (val != MSM_DRM_EVENT_BLANK) {
 		return 0;
 	}
 
@@ -753,7 +753,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		 __func__, (int)val);
 	gf_dev = container_of(nb, struct gf_dev, notifier);
 
-	if (evdata && evdata->data && val == DRM_EVENT_BLANK && gf_dev) {
+	if (evdata && evdata->data && val == MSM_DRM_EVENT_BLANK && gf_dev) {
 		blank = *(int *)(evdata->data);
 
 		switch (blank) {
