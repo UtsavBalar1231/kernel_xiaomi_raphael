0c741ed294c5180aca07f78ef7c8d4596d87b967
Yong Ding <yongding@codeaurora.org>
2019-12-20 17:47:54 +0800

    Revert "soc: qcom: hab: use dma buf as default implement of export/import"
    This reverts commit 253b79d2434032d78717d366e255a1687e66c28f.
    
    Change-Id: Ic5f485f14b518e9905209a41c7f8f0e2386e8e9a
    Signed-off-by: Yong Ding <yongding@codeaurora.org>

--- drivers/soc/qcom/hab/hab.c
+++ drivers/soc/qcom/hab/hab.c
@@ -56,8 +56,6 @@ struct hab_driver hab_driver = {
 	.devp = hab_devices,
 	.uctx_list = LIST_HEAD_INIT(hab_driver.uctx_list),
 	.drvlock = __SPIN_LOCK_UNLOCKED(hab_driver.drvlock),
-	.imp_list = LIST_HEAD_INIT(hab_driver.imp_list),
-	.imp_lock = __SPIN_LOCK_UNLOCKED(hab_driver.imp_lock),
 };
 
 struct uhab_context *hab_ctx_alloc(int kernel)
@@ -114,8 +112,7 @@ void hab_ctx_free(struct kref *ref)
 	int i;
 	struct uhab_context *ctxdel, *ctxtmp;
 	struct hab_open_node *node;
-	struct export_desc *exp = NULL,
-			*exp_tmp = NULL;
+	struct export_desc *exp, *exp_tmp;
 
 	/* garbage-collect exp/imp buffers */
 	write_lock_bh(&ctx->exp_lock);
