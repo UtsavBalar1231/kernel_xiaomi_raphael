f5ab2259cbae277bec56d3c26be453d8fdaa1e1e
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-23 18:11:02 +0000

    Revert "drivers: video: apply backlight event notifier changes"
    This reverts commit 9282c60e3ee5cd19c4c7de2746f08d3517623f80.

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -24,7 +24,6 @@
 static struct list_head backlight_dev_list;
 static struct mutex backlight_dev_list_mutex;
 static struct blocking_notifier_head backlight_notifier;
-static int bl_event;
 
 static const char *const backlight_types[] = {
 	[BACKLIGHT_RAW] = "raw",
@@ -192,13 +191,6 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 			}
 
 			pr_debug("set brightness to %lu\n", brightness);
-			if (!brightness) {
-				bl_event = BACKLIGHT_OFF;
-				blocking_notifier_call_chain(&backlight_notifier, BACKLIGHT_UPDATED, &bl_event);
-			} else if (bl_event != BACKLIGHT_ON) {
-				bl_event = BACKLIGHT_ON;
-				blocking_notifier_call_chain(&backlight_notifier, BACKLIGHT_UPDATED, &bl_event);
-			}
 			bd->props.brightness = brightness;
 			rc = backlight_update_status(bd);
 		}
