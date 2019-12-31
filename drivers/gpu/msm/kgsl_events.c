5db6ea119ef076d72456848743c83d750671250a
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-31 04:25:18 +0000

    drivers: gpu: msm: remove unused variables
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/gpu/msm/kgsl_events.c
+++ drivers/gpu/msm/kgsl_events.c
@@ -61,7 +61,6 @@ const char *prio_to_string(enum kgsl_priority prio)
 static void _kgsl_event_worker(struct kthread_work *work)
 {
 	struct kgsl_event *event = container_of(work, struct kgsl_event, work);
-	int id = KGSL_CONTEXT_ID(event->context);
 
 	trace_kgsl_fire_event(id, event->timestamp, event->result,
 		jiffies - event->created, event->func, event->prio);
