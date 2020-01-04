6d8e0045d2d9bbed832045f66141408af2d056d4
UtsavisGreat <utsavbalar1231@gmail.com>
2020-01-04 04:24:46 +0000

    drm: msm: remove esd irq handling
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -165,9 +165,6 @@ struct drm_panel_esd_config {
 	u8 *return_buf;
 	u8 *status_buf;
 	u32 groups;
-	int esd_err_irq_gpio;
-	int esd_err_irq;
-	int esd_err_irq_flags;
 };
 
 struct dsi_read_config {
