c50ce363440595552573572ef30508ebd8dacea1
UtsavisGreat <utsavbalar1231@gmail.com>
2020-01-09 12:31:38 +0000

    Revert "selinux: reduce calls to context_struct_to_string()"
    This reverts commit f283f9507bb99e42cb17a70871040554b22c12fb.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -456,7 +456,6 @@ mls_ops:
 	return s[0];
 }
 
-#ifdef CONFIG_AUDIT
 /*
  * security_dump_masked_av - dumps masked permissions during
  * security_compute_av due to RBAC, MLS/Constraint and Type bounds.
@@ -546,7 +545,6 @@ out:
 
 	return;
 }
-#endif
 
 /*
  * security_boundary_permission - drops violated permissions
@@ -600,11 +598,9 @@ static void type_attribute_bounds_av(struct context *scontext,
 	/* mask violated permissions */
 	avd->allowed &= ~masked;
 
-#ifdef CONFIG_AUDIT
 	/* audit masked permissions */
 	security_dump_masked_av(scontext, tcontext,
 				tclass, masked, "bounds");
-#endif
 }
 
 /*
@@ -741,12 +737,11 @@ static void context_struct_compute_av(struct context *scontext,
 				 tclass, avd);
 }
 
-static inline int security_validtrans_handle_fail(struct context *ocontext,
+static int security_validtrans_handle_fail(struct context *ocontext,
 					   struct context *ncontext,
 					   struct context *tcontext,
 					   u16 tclass)
 {
-#ifdef CONFIG_AUDIT
 	char *o = NULL, *n = NULL, *t = NULL;
 	u32 olen, nlen, tlen;
 
@@ -764,7 +759,6 @@ out:
 	kfree(o);
 	kfree(n);
 	kfree(t);
-#endif
 
 	if (!selinux_enforcing)
 		return 0;
@@ -918,7 +912,6 @@ int security_bounded_transition(u32 old_sid, u32 new_sid)
 		index = type->bounds;
 	}
 
-#ifdef CONFIG_AUDIT
 	if (rc) {
 		char *old_name = NULL;
 		char *new_name = NULL;
@@ -938,7 +931,6 @@ int security_bounded_transition(u32 old_sid, u32 new_sid)
 		kfree(new_name);
 		kfree(old_name);
 	}
-#endif
 out:
 	read_unlock(&policy_rwlock);
 
@@ -1526,13 +1518,12 @@ int security_context_to_sid_force(const char *scontext, u32 scontext_len,
 					    sid, SECSID_NULL, GFP_KERNEL, 1);
 }
 
-static inline int compute_sid_handle_invalid_context(
+static int compute_sid_handle_invalid_context(
 	struct context *scontext,
 	struct context *tcontext,
 	u16 tclass,
 	struct context *newcontext)
 {
-#ifdef CONFIG_AUDIT
 	char *s = NULL, *t = NULL, *n = NULL;
 	u32 slen, tlen, nlen;
 
@@ -1552,8 +1543,6 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
-#endif
-
 	if (!selinux_enforcing)
 		return 0;
 	return -EACCES;
@@ -1842,20 +1831,16 @@ static int clone_sid(u32 sid,
 
 static inline int convert_context_handle_invalid_context(struct context *context)
 {
-#ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
-#endif
 
 	if (selinux_enforcing)
 		return -EINVAL;
 
-#ifdef CONFIG_AUDIT
 	if (!context_struct_to_string(context, &s, &len)) {
 		printk(KERN_WARNING "SELinux:  Context %s would be invalid if enforcing\n", s);
 		kfree(s);
 	}
-#endif
 	return 0;
 }
 
@@ -1883,10 +1868,8 @@ static int convert_context(u32 key,
 	struct type_datum *typdatum;
 	struct user_datum *usrdatum;
 	char *s;
-	int rc = 0;
-#ifdef CONFIG_AUDIT
 	u32 len;
-#endif
+	int rc = 0;
 
 	if (key <= SECINITSID_NUM)
 		goto out;
@@ -1999,7 +1982,6 @@ static int convert_context(u32 key,
 out:
 	return rc;
 bad:
-#ifdef CONFIG_AUDIT
 	/* Map old representation to string and save it. */
 	rc = context_struct_to_string(&oldc, &s, &len);
 	if (rc)
@@ -2012,9 +1994,6 @@ bad:
 	       c->str);
 	rc = 0;
 	goto out;
-#else
-	return 0;
-#endif
 }
 
 static void security_load_policycaps(void)
@@ -2870,11 +2849,9 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 	struct context *context1;
 	struct context *context2;
 	struct context newcon;
-	int rc;
-#ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
-#endif
+	int rc;
 
 	rc = 0;
 	if (!ss_initialized || !policydb.mls_enabled) {
@@ -2913,7 +2890,6 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 	if (!policydb_context_isvalid(&policydb, &newcon)) {
 		rc = convert_context_handle_invalid_context(&newcon);
 		if (rc) {
-#ifdef CONFIG_AUDIT
 			if (!context_struct_to_string(&newcon, &s, &len)) {
 				audit_log(current->audit_context,
 					  GFP_ATOMIC, AUDIT_SELINUX_ERR,
@@ -2921,7 +2897,6 @@ int security_sid_mls_copy(u32 sid, u32 mls_sid, u32 *new_sid)
 					  "invalid_context=%s", s);
 				kfree(s);
 			}
-#endif
 			goto out_unlock;
 		}
 	}
