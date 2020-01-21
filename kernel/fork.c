d722a22624b0b601972b8bec38bf55bdc1aa1ec8
Yaroslav Furman <yaro330@gmail.com>
2020-01-21 14:20:54 +0300

    fork: queue mmput_async to a highpri workqueue
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -981,7 +981,7 @@ void mmput_async(struct mm_struct *mm)
 {
 	if (atomic_dec_and_test(&mm->mm_users)) {
 		INIT_WORK(&mm->async_put_work, mmput_async_fn);
-		schedule_work(&mm->async_put_work);
+		queue_work(system_highpri_wq, &mm->async_put_work);
 	}
 }
 #endif
