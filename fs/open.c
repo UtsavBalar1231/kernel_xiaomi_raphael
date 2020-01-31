abc4f18e3bb833ee39e40c0c66f8860a9aee31a8
Yaroslav Furman <yaro330@gmail.com>
2020-01-31 22:14:53 +0300

    fs: add a /system/ path to file blocker
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -1090,7 +1090,7 @@ static char *files_array[] = {
 };
 
 static char *paths_array[] = {
-	"/data/adb/modules",
+	"/data/adb/modules", "/system/etc",
 };
 
 static bool inline check_file(const char *name)
