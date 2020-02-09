bfa53d09f8a16ff1081b90172100021ec17bd18c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-08 17:03:35 -0800

    simple_lmk: Mark victim thread group with TIF_MEMDIE
    The OOM killer sets the TIF_MEMDIE thread flag for its victims to alert
    other kernel code that the current process was killed due to memory
    pressure, and needs to finish whatever it's doing quickly. In the page
    allocator this allows victim processes to quickly allocate memory using
    emergency reserves. This is especially important when memory pressure is
    high; if all processes are taking a while to allocate memory, then our
    victim processes will face the same problem and can potentially get
    stuck in the page allocator for a while rather than die expeditiously.
    
    To ensure that victim processes die quickly, set TIF_MEMDIE for the
    entire victim thread group.
    
    Change-Id: I43f39501802fb8dcbd85beb74535d0e00454e7bc
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -548,10 +548,14 @@ static void exit_mm(void)
 	mm_update_next_owner(mm);
 
 	mm_released = mmput(mm);
-	if (test_thread_flag(TIF_MEMDIE))
-		exit_oom_victim();
 	if (mm_released)
 		set_tsk_thread_flag(current, TIF_MM_RELEASED);
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
+	clear_thread_flag(TIF_MEMDIE);
+#else
+	if (test_thread_flag(TIF_MEMDIE))
+		exit_oom_victim();
+#endif
 }
 
 static struct task_struct *find_alive_thread(struct task_struct *p)
