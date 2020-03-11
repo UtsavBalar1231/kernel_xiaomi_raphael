33a86217abd5665f66ab8f72f0cef3b9d88b71e5
Sultan Alsawaf <sultan@kerneltoast.com>
2020-03-10 19:53:02 -0700

    ext4: Allocate 128-byte allocation context on the stack
    The allocation context structure is 128 bytes and thus easily fits on
    the stack. There's no need to allocate it dynamically.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -349,7 +349,6 @@ MODULE_PARM_DESC(mballoc_debug, "Debugging level for ext4's mballoc");
  *
  */
 static struct kmem_cache *ext4_pspace_cachep;
-static struct kmem_cache *ext4_ac_cachep;
 static struct kmem_cache *ext4_free_data_cachep;
 
 /* We create slab caches for groupinfo data structures based on the
@@ -2953,7 +2952,6 @@ void ext4_exit_mballoc(void)
 	 */
 	rcu_barrier();
 	kmem_cache_destroy(ext4_pspace_cachep);
-	kmem_cache_destroy(ext4_ac_cachep);
 	kmem_cache_destroy(ext4_free_data_cachep);
 	ext4_groupinfo_destroy_slabs();
 }
