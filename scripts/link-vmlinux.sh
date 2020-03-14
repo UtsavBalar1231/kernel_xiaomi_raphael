8b229343d992104846e0b60b699aae1e5f05b57c
Yousef Algadri <yusufgadrie@gmail.com>
2020-03-14 21:08:57 +0700

    vmlinux: Don't use LDFINAL for clang
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: Ifc529c6614300102c2eae9f106b814a109f32c2b
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -125,7 +125,11 @@ modpost_link()
 		info LD vmlinux.o
 	fi
 
-	${LDFINAL} ${LDFLAGS} -r -o ${1} $(lto_lds) ${objects}
+	if [ -n "${CONFIG_GCC_LTO}" ]; then
+		${LDFINAL} ${LDFLAGS} -r -o ${1} $(lto_lds) ${objects}
+	else
+		${LD} ${LDFLAGS} -r -o ${1} $(lto_lds) ${objects}
+	fi
 }
 
 # If CONFIG_LTO_CLANG is selected, we postpone running recordmcount until
@@ -434,7 +438,12 @@ if [ ! -z ${RTIC_MP_O} ]; then
 	fi
 fi
 
-info LDFINAL vmlinux
+if [ -n "${CONFIG_GCC_LTO}" ]; then
+	info LDFINAL vmlinux
+else
+	info LD vmlinux
+fi
+
 vmlinux_link "${kallsymso}" vmlinux
 
 if [ -n "${CONFIG_BUILDTIME_EXTABLE_SORT}" ]; then
