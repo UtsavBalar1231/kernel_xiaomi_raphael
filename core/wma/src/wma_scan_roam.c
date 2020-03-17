56fa73b2e1153bdd1870cc7b77aecc102e9c0095
Abhishek Ambure <aambure@codeaurora.org>
2020-03-17 21:14:10 +0530

    qcacld-3.0: Optimize LFR logs
    Optimize LFR3 logs.
    
    Change-Id: I13067dfa504dd9310eff48b138916da2791cb270
    CRs-Fixed: 2643684

--- core/wma/src/wma_scan_roam.c
+++ core/wma/src/wma_scan_roam.c
@@ -2786,7 +2786,7 @@ int wma_mlme_roam_synch_event_handler_cb(void *handle, uint8_t *event,
 	A_UINT32 reassoc_rsp_len;
 	A_UINT32 reassoc_req_len;
 
-	WMA_LOGD("LFR3:%s", __func__);
+	WMA_LOGD("LFR3: Received WMA_ROAM_OFFLOAD_SYNCH_IND");
 	if (!event) {
 		WMA_LOGE("%s: event param null", __func__);
 		goto cleanup_label;
@@ -2844,7 +2844,6 @@ int wma_mlme_roam_synch_event_handler_cb(void *handle, uint8_t *event,
 				__func__);
 		goto cleanup_label;
 	}
-	WMA_LOGD("LFR3: Received WMA_ROAM_OFFLOAD_SYNCH_IND");
 
 	/*
 	 * All below length fields are unsigned and hence positive numbers.
@@ -2908,11 +2907,6 @@ int wma_mlme_roam_synch_event_handler_cb(void *handle, uint8_t *event,
 
 		roam_synch_data_len += sizeof(struct roam_offload_synch_ind);
 	}
-
-	WMA_LOGD("synch payload: LEN bcn:%d, req:%d, rsp:%d",
-			bcn_probe_rsp_len,
-			reassoc_req_len,
-			reassoc_rsp_len);
 
 	cds_host_diag_log_work(&wma->roam_ho_wl,
 			       WMA_ROAM_HO_WAKE_LOCK_DURATION,
@@ -3010,6 +3004,9 @@ cleanup_label:
 			roam_req->Command = ROAM_SCAN_OFFLOAD_STOP;
 			roam_req->reason = REASON_ROAM_SYNCH_FAILED;
 			roam_req->sessionId = synch_event->vdev_id;
+			wma_debug("In cleanup: RSO Command:%d, reason %d vdev %d",
+				  roam_req->Command, roam_req->reason,
+				  roam_req->sessionId);
 			wma_process_roaming_config(wma, roam_req);
 		}
 	}
@@ -6144,7 +6141,6 @@ int wma_roam_event_callback(WMA_HANDLE handle, uint8_t *event_buf,
 	case WMI_ROAM_REASON_DEAUTH:
 		WMA_LOGD("%s: Received disconnect roam event reason:%d",
 			 __func__, wmi_event->notif_params);
-
 		if (wmi_event->notif_params1)
 			frame = param_buf->deauth_disassoc_frame;
 		wma_handle->pe_disconnect_cb(wma_handle->mac_context,
