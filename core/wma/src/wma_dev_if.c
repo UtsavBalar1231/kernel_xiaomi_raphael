bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Add firmware service support for flush tids
Firmware advertises service capability bit
WMI_SERVICE_PEER_DELETE_NO_PEER_FLUSH_TIDS_CMD
when peer delete command takes care of flush
all tids functionality.
If this service bit is set advertised by FW
then host does not need to send the flush tid
command before sending the peer delete command.

Change-Id: I7838b1d0465fefd58eb8faf1d2d87beccf55134b
CRs-Fixed: 2609964

core/wma/src/wma_dev_if.c
@@ -1584,8 +1584,10 @@ QDF_STATUS wma_remove_peer(tp_wma_handle wma, uint8_t *mac_addr,
 	peer_tid_bitmap &= ~(0x1 << WMI_MGMT_TID);
 	param.peer_tid_bitmap = peer_tid_bitmap;
 	param.vdev_id = vdev_id;
-	wmi_unified_peer_flush_tids_send(wma->wmi_handle, mac_addr,
-					 &param);
+	if (!wmi_service_enabled(wma->wmi_handle,
+				 wmi_service_peer_delete_no_peer_flush_tids_cmd))
+		wmi_unified_peer_flush_tids_send(wma->wmi_handle, mac_addr,
+						 &param);
 
 	/* peer->ref_cnt is not visible in WMA */
 	wlan_roam_debug_log(vdev_id, DEBUG_PEER_DELETE_SEND,

UtsavBalar1231/kernel_xiaomi_raphael

core/wma/src/wma_dev_if.c
@@ -1887,7 +1887,10 @@ QDF_STATUS wma_remove_peer(tp_wma_handle wma, uint8_t *bssid,
 	peer_tid_bitmap &= ~(0x1 << WMI_MGMT_TID);
 	param.peer_tid_bitmap = peer_tid_bitmap;
 	param.vdev_id = vdev_id;
-	wmi_unified_peer_flush_tids_send(wma->wmi_handle, bssid,
+
+	if (!wmi_service_enabled(wma->wmi_handle,
+				 wmi_service_peer_delete_no_peer_flush_tids_cmd))
+		wmi_unified_peer_flush_tids_send(wma->wmi_handle, bssid,
 			&param);
 
 	if (wma_is_vdev_in_ibss_mode(wma, vdev_id)) {