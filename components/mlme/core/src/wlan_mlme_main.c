9c58cec4d01ed5017f1981536812203e2a2baf91
gaurank kathpalia <gkathpal@codeaurora.org>
2020-03-26 22:14:15 +0530

    qcacld-3.0: Add ini to enable/disable self roaming
    Currently the driver allows roaming to the BSSID
    which it is already connected, which can lead to
    many unexpected behaviour, for eg. if the SAP
    to which the DUT is connected turns off, then the
    DUT would try to roam to other BSSID of the same
    SSID, and in case the SAP turns on back in time,
    then the DUT would re-associate to the same BSSID.
    Now if the subnet may change, and since the DHCP
    does not happen again in roaming, it would then
    lead to NUD failure.
    
    Fix is to introduce an ini to allow/disallow
    roaming to the same BSSID.
    
    Change-Id: Ibd04fcd7384c0987db5bb6dae1a26ee077456290
    CRs-Fixed: 2634860

--- components/mlme/core/src/wlan_mlme_main.c
+++ components/mlme/core/src/wlan_mlme_main.c
@@ -1478,7 +1478,7 @@ static void mlme_init_roam_offload_cfg(struct wlan_objmgr_psoc *psoc,
 {
 	lfr->lfr3_roaming_offload =
 		cfg_get(psoc, CFG_LFR3_ROAMING_OFFLOAD);
-
+	lfr->enable_self_bss_roam = cfg_get(psoc, CFG_LFR3_ENABLE_SELF_BSS_ROAM);
 	lfr->enable_disconnect_roam_offload =
 		cfg_get(psoc, CFG_LFR_ENABLE_DISCONNECT_ROAM);
 	lfr->enable_idle_roam =
