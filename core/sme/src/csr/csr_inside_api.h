3e57275d2fa5a95f56c62052166580cc2a019f1c
hqu <hqu@codeaurora.org>
2020-04-10 20:22:50 +0800

    qcacld-3.0: Don't remove pending cmd when NB disconnect complete
    In case eCsrForcedDeauthSta from NB is completed, eWNI_SME_DEAUTH_RSP
    will remove all pending eSmeCommandWmStatusChange type commands from
    serialization, this will cause other peers which deauth request is
    from SB are not deleted from LIM and remain in deleting progress.
    
    Fix is to don't remove pending eSmeCommandWmStatusChange type
    commands when NB disconnect complete from eWNI_SME_DEAUTH_RSP.
    
    Change-Id: I8e2248463fb7fd97343d411d46b1cae08dc4cb25
    CRs-Fixed: 2652486

--- core/sme/src/csr/csr_inside_api.h
+++ core/sme/src/csr/csr_inside_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -197,8 +197,6 @@ QDF_STATUS csr_scan_for_ssid(struct mac_context *mac, uint32_t sessionId,
  */
 QDF_STATUS csr_scan_abort_mac_scan(struct mac_context *mac, uint32_t vdev_id,
 				   uint32_t scan_id);
-QDF_STATUS csr_remove_nonscan_cmd_from_pending_list(struct mac_context *mac,
-			uint8_t sessionId, eSmeCommandType commandType);
 
 /* If fForce is true we will save the new String that is learn't. */
 /* Typically it will be true in case of Join or user initiated ioctl */
