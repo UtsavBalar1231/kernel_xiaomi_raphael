11f30ca70fc636181f21d28d573824a3833d77e2
Lakshit Tyagi <ltyagi@codeaurora.org>
2020-04-17 15:44:26 +0530

    data-kernel: Low level IPC logs
    - Correct the copyright year
    
    Signed-off-by: Lakshit Tyagi <ltyagi@codeaurora.org>
    Change-Id: Iac4d2162d12a37d69b11db6c4c75d8f1f088069d

--- drivers/emac-dwc-eqos/DWC_ETH_QOS_ipc.h
+++ drivers/emac-dwc-eqos/DWC_ETH_QOS_ipc.h
@@ -1,6 +1,6 @@
 /***************************************************************************
 
-* Copyright (c) 2019-20, The Linux Foundation. All rights reserved.
+* Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
 
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
