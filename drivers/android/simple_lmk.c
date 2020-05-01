3e7a5ce21931c547a485553737edfaefadefbc03
Yaroslav Furman <yaro330@gmail.com>
2020-05-01 21:00:54 +0300

    simple_lmk: do not flood the log when we're stuck
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -183,7 +183,7 @@ static void scan_and_kill(unsigned long pages_needed)
 
 	/* Pretty unlikely but it can happen */
 	if (unlikely(!nr_found)) {
-		pr_err("No processes available to kill!\n");
+		printk_once("No processes available to kill!\n");
 		return;
 	}
 
