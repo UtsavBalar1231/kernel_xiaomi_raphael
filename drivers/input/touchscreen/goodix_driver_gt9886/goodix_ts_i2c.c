890be573cc2b093986355d7b6d170c8925e5d6f9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-06-11 09:42:46 +0530

    touchscreen: goodix_driver_gt9886: Change pressure reporting
    - Update Hand touch pressure level 0~4096, size: 0~256.
    
    Change-Id: Iad98efdacf742147548e1cb6efa117d41a5239b4
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_i2c.c
+++ drivers/input/touchscreen/goodix_driver_gt9886/goodix_ts_i2c.c
@@ -1840,8 +1840,9 @@ static int goodix_touch_handler(struct goodix_ts_device *dev,
 							(buffer[i * BYTES_PER_COORD + 4] << 8);
 			coords->y = buffer[i * BYTES_PER_COORD + 5] |
 							(buffer[i * BYTES_PER_COORD + 6] << 8);
-			coords->w = buffer[i * BYTES_PER_COORD + 7];
-			coords->p = coords->w;
+			coords->w = coords->p / 16;
+			coords->p = buffer[i * BYTES_PER_COORD + 7] |
+			(buffer[i * BYTES_PER_COORD + 8] << 8);
 			coords->overlapping_area = buffer[8];
 			coords->area = buffer[i * BYTES_PER_COORD + 9];
 			coords++;
@@ -1861,8 +1862,10 @@ static int goodix_touch_handler(struct goodix_ts_device *dev,
 					(buffer[i * BYTES_PER_COORD + 4] << 8);
 				touch_data->pen_coords[0].y = buffer[i * BYTES_PER_COORD + 5] |
 					(buffer[i * BYTES_PER_COORD + 6] << 8);
-				touch_data->pen_coords[0].w = buffer[i * BYTES_PER_COORD + 7];
-				touch_data->pen_coords[0].p = touch_data->pen_coords[0].w;
+				touch_data->pen_coords[0].w = 6;
+				touch_data->pen_coords[0].p =
+					buffer[i * BYTES_PER_COORD + 7] |
+					(buffer[i * BYTES_PER_COORD + 8] << 8);
 				}
 			} else {/*it's a finger*/
 					coords->id = buffer[i * BYTES_PER_COORD + 2] & 0x0f;
@@ -1870,8 +1873,9 @@ static int goodix_touch_handler(struct goodix_ts_device *dev,
 									(buffer[i * BYTES_PER_COORD + 4] << 8);
 					coords->y = buffer[i * BYTES_PER_COORD + 5] |
 									(buffer[i * BYTES_PER_COORD + 6] << 8);
-					coords->w = buffer[i * BYTES_PER_COORD + 7];
-					coords->p = coords->w;
+					coords->w = coords->p / 16;
+					coords->p = buffer[i * BYTES_PER_COORD + 7] |
+							(buffer[i * BYTES_PER_COORD + 8] << 8);
 					coords->overlapping_area = buffer[8];
 					coords->area = buffer[i * BYTES_PER_COORD + 9];
 					/*ts_debug("EF:[%d](%d, %d)", coords->id, coords->x, coords->y);*/
