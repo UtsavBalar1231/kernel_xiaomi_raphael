6343a446d7382dc0766f0ca55ea7877467ad2522
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-06-14 20:19:34 +0530

    Revert "fscrypt: invoke crypto API for ESSIV handling"
    This reverts commit 17d43f2bf98c75afe28fdc5f7032b43df2fac9ce.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/crypto/fscrypt_private.h
+++ fs/crypto/fscrypt_private.h
@@ -165,6 +165,12 @@ struct fscrypt_info {
 	struct crypto_skcipher *ci_ctfm;
 
 	/*
+	 * Cipher for ESSIV IV generation.  Only set for CBC contents
+	 * encryption, otherwise is NULL.
+	 */
+	struct crypto_cipher *ci_essiv_tfm;
+
+	/*
 	 * Encryption mode used for this inode.  It corresponds to either the
 	 * contents or filenames encryption mode, depending on the inode type.
 	 */
@@ -419,6 +425,7 @@ struct fscrypt_mode {
 	int keysize;
 	int ivsize;
 	bool logged_impl_name;
+	bool needs_essiv;
 };
 
 static inline bool
