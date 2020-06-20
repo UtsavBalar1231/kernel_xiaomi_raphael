74d5e8c0b7d6d5bfa1337cd19a5f60f27386c922
Yaroslav Furman <yaro330@gmail.com>
2020-06-20 16:20:49 +0300

    msm/sde_fence: ifdef name from sde_fence_context struct
    It isn't used as we have https://github.com/F1xy-kernels/RAPHAEL_Kernel/commit/d24272a6a59b4da38551ec186200796073bb38e4 applied.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_fence.h
+++ drivers/gpu/drm/msm/sde/sde_fence.h
@@ -44,7 +44,9 @@ struct sde_fence_context {
 	spinlock_t list_lock;
 	u64 context;
 	struct list_head fence_list_head;
+#ifdef CONFIG_FENCE_DEBUG
 	char name[SDE_FENCE_NAME_SIZE];
+#endif
 };
 
 /**
