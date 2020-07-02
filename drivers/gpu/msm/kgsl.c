b1a358e778a6262bba65fc650d2d7822f1882973
Hareesh Gundu <hareeshg@codeaurora.org>
2020-07-02 14:18:49 +0530

    msm: kgsl: Increase events worker thread priority
    Increase kgsl-events worker thread priority to run at high
    priority among default worker threads to get some benefit
    in fence signaling.
    
    Change-Id: Ia3eeb1beabc4bca1ef69e37bc40ef9868e29572b
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5074,7 +5074,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_DEFAULT_VALUE);
 
 	device->events_wq = alloc_workqueue("kgsl-events",
-		WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS, 0);
+		WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS | WQ_HIGHPRI, 0);
 
 	/* Initialize the snapshot engine */
 	kgsl_device_snapshot_init(device);
