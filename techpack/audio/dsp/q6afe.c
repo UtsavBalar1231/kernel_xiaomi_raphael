00b4d24376c393c83326b0c8a91369db49b9d36f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-07-04 18:28:54 +0530

    Revert "techpack: fix the rest of debug_fs related errors"
    This reverts commit 43d6834364aba5649cdc95fb13c822cc50252821.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -8647,7 +8647,7 @@ static void afe_release_uevent_data(struct kobject *kobj)
 
 #ifdef CONFIG_SND_SOC_TFA9874_FOR_DAVI
 
-extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
+int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
 {
 	int32_t result = 0, port_id = AFE_PORT_ID_TFADSP_RX;
 	uint32_t port_index = 0, payload_size = 0;
@@ -8769,6 +8769,7 @@ extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
 err:
 	return result;
 }
+EXPORT_SYMBOL(send_tfa_cal_apr);
 
 void send_tfa_cal_unmap_memory(void)
 {
