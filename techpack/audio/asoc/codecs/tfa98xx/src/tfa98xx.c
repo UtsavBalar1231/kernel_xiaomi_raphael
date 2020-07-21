4dcbee3b01cdb9a3b5d0b85c623775feffa5ae2f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-07-21 17:23:32 +0530

    tfa98xx: allow SKIP_INTERRUPTS feature
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
@@ -4149,6 +4149,7 @@ static int tfa98xx_i2c_probe(struct i2c_client *i2c,
 			pr_info("TFA9874 detected\n");
 			tfa98xx->flags |= TFA98XX_FLAG_MULTI_MIC_INPUTS;
 			tfa98xx->flags |= TFA98XX_FLAG_CALIBRATION_CTL;
+			tfa98xx->flags |= TFA98XX_FLAG_SKIP_INTERRUPTS;
 			tfa98xx->flags |= TFA98XX_FLAG_TDM_DEVICE;
 			break;
 		case 0x78: /* tfa9878 */
