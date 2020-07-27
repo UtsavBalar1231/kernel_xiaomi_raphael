87d1cb47585722a60a0b1c14932bee8f9af72da4
Yaroslav Furman <yaro330@gmail.com>
2020-07-27 03:58:47 +0300

    techpack: wcd-mbhc-v2: make sure headset button wakes the system up
    - Add IRQF_NO_SUSPEND to button IRQs (press/release).
    - Add wakeups to button press/release functions.
    
    These changes ensure that system is woken up when you
    pause playback, wait for a bit, try to resume playback
    using headset's button.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/audio/asoc/codecs/wcd-mbhc-v2.c
+++ techpack/audio/asoc/codecs/wcd-mbhc-v2.c
@@ -36,6 +36,7 @@
 #include "wcd-mbhc-v2-api.h"
 
 #define CONFIG_AUDIO_UART_DEBUG
+static struct wakeup_source mbhc_ws;
 
 void wcd_mbhc_jack_report(struct wcd_mbhc *mbhc,
 			  struct snd_soc_jack *jack, int status, int mask)
@@ -1171,6 +1172,7 @@ static irqreturn_t wcd_mbhc_btn_press_handler(int irq, void *data)
 	unsigned long msec_val;
 
 	pr_debug("%s: enter\n", __func__);
+	__pm_wakeup_event(&mbhc_ws, 500);
 	complete(&mbhc->btn_press_compl);
 	WCD_MBHC_RSC_LOCK(mbhc);
 	wcd_cancel_btn_work(mbhc);
@@ -1221,6 +1223,7 @@ static irqreturn_t wcd_mbhc_release_handler(int irq, void *data)
 	int ret;
 
 	pr_debug("%s: enter\n", __func__);
+	__pm_wakeup_event(&mbhc_ws, 500);
 	WCD_MBHC_RSC_LOCK(mbhc);
 	if (wcd_swch_level_remove(mbhc)) {
 		pr_debug("%s: Switch level is low ", __func__);
@@ -2236,6 +2239,7 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 		       mbhc->intr_ids->hph_right_ocp);
 		goto err_hphr_ocp_irq;
 	}
+	wakeup_source_init(&mbhc_ws, "mbhc_ws");
 
 	mbhc->deinit_in_progress = false;
 	pr_debug("%s: leave ret %d\n", __func__, ret);
@@ -2260,6 +2264,7 @@ err_mbhc_sw_irq:
 		mbhc->mbhc_cb->register_notifier(mbhc, &mbhc->nblock, false);
 	mutex_destroy(&mbhc->codec_resource_lock);
 err:
+	wakeup_source_trash(&mbhc_ws);
 	pr_debug("%s: leave ret %d\n", __func__, ret);
 	return ret;
 }
