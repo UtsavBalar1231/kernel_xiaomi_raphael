d9a6307fd484f4d97e4ab83fcccc6656ba479cdc
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-07-30 13:44:10 +0530

    scsi: sd: fix a massive performance regression
    - this fixes a major storage performance
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3120,10 +3120,10 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	dev_max = min_not_zero(dev_max, sdkp->max_xfer_blocks);
 	q->limits.max_dev_sectors = logical_to_sectors(sdp, dev_max);
 
-	if (sd_validate_opt_xfer_size(sdkp, dev_max)) {
-		q->limits.io_opt = logical_to_bytes(sdp, sdkp->opt_xfer_blocks);
-		rw_max = logical_to_sectors(sdp, sdkp->opt_xfer_blocks);
-	} else {
+	if (sd_validate_opt_xfer_size(sdkp, dev_max))
+		rw_max = q->limits.io_opt =
+				sdkp->opt_xfer_blocks * sdp->sector_size;
+	else {
 		q->limits.io_opt = 0;
 		rw_max = min_not_zero(logical_to_sectors(sdp, dev_max),
 				      (sector_t)BLK_DEF_MAX_SECTORS);
