da10ec15d9bd3608a4295412779531698884a117
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-03 14:10:26 +0530

    dsi_panel: Do not explicitly enable ULPS mode during suspend
    This reverts commit 36f66843835d5992c4b87efa76ce8320505d6402.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2296,7 +2296,8 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	pr_info("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
 
-	panel->ulps_suspend_enabled = true;
+	panel->ulps_suspend_enabled =
+		utils->read_bool(utils->data, "qcom,suspend-ulps-enabled");
 
 	pr_info("%s: ulps during suspend feature %s", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
