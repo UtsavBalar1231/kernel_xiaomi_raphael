8d75136fefcf60bf7c50d1138b06a34cae72dfa6
Anurag Chouhan <achouhan@codeaurora.org>
2020-08-24 13:36:29 +0530

    soc: qcom: socinfo: Remove Unnecessary soc-id
    Remove Unnecessary socinfo support and update the
    bindings for the same.
    
    Change-Id: I724b37fd7e466307db0c209cef834c4a308fa940
    Signed-off-by: Anurag Chouhan <achouhan@codeaurora.org>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -107,10 +107,6 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,atollp")
 #define early_machine_is_atoll_ab()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,atoll-ab")
-#define early_machine_is_qcm6125()       \
-	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,qcm6125")
-#define early_machine_is_qcs6125()       \
-	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,qcs6125")
 #else
 #define of_board_is_sim()		0
 #define of_board_is_rumi()		0
@@ -153,8 +149,6 @@
 #define early_machine_is_qcs410()       0
 #define early_machine_is_atoll()	0
 #define early_machine_is_atollp()	0
-#define early_machine_is_qcm6125()	0
-#define early_machine_is_qcs6125()	0
 #endif
 
 #define PLATFORM_SUBTYPE_MDM	1
@@ -200,8 +194,6 @@ enum msm_cpu {
 	MSM_CPU_ATOLL,
 	MSM_CPU_ATOLLP,
 	MSM_CPU_ATOLL_AB,
-	MSM_CPU_QCM6125,
-	MSM_CPU_QCS6125,
 };
 
 struct msm_soc_info {
