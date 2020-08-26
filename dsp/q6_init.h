fb5ccbba29d1cc9b8d8ca232ef6c29d516c535a9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-26 17:38:17 +0530

    ASoC: Add Support for Elliptic US proximity driver
    - Extracted from MiCode/vendor_qcom_opensource_audio-kernel
      at branch 'cepheus-q-oss'
    - Fix code to make it compile inline
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- dsp/q6_init.h
+++ dsp/q6_init.h
@@ -30,6 +30,9 @@ int avtimer_init(void);
 int msm_mdf_init(void);
 void msm_mdf_exit(void);
 #else
+#ifdef CONFIG_ELLIPTIC_ULTRASOUND
+int elliptic_driver_init(void);
+#endif
 static inline int msm_mdf_init(void)
 {
 	return 0;
@@ -65,6 +68,9 @@ void q6asm_exit(void);
 void afe_exit(void);
 void adm_exit(void);
 void adsp_err_exit(void);
+#ifdef CONFIG_ELLIPTIC_ULTRASOUND
+int elliptic_driver_exit(void);
+#endif
 
 #ifdef CONFIG_VOICE_MHI
 int voice_mhi_init(void);
