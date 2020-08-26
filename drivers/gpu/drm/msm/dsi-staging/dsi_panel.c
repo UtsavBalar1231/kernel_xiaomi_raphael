7499298fcfb9e7918e6880133a805d1d7fb1e36e
Arian <arian.kulmer@web.de>
2020-08-26 16:01:36 +0200

    drm: msm: Skip fod_hbm while HBM is active
    Change-Id: I84409400b4ba33370a32e8e9cbe87b3ad9dfb83c

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -716,6 +716,9 @@ static u32 interpolate(uint32_t x, uint32_t xa, uint32_t xb, uint32_t ya, uint32
 
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 {
+	if (panel->hbm_mode)
+		return 0;
+
 	u32 brightness = dsi_panel_get_backlight(panel);
 	int i;
 
@@ -784,6 +787,9 @@ int dsi_panel_set_doze_mode(struct dsi_panel *panel, enum dsi_doze_mode_type mod
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 {
 	int rc = 0;
+
+	if (panel->hbm_mode)
+		return rc;
 
 	if (status) {
 #ifdef CONFIG_EXPOSURE_ADJUSTMENT
