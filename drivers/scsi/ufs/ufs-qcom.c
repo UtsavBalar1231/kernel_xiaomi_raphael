7b083a304ba7efaf4aa93de737787bc362d43bd3
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-28 19:54:57 +0530

    Revert "Variant ops for UFS crypto and new crypto lib"
    This reverts commit e62b481e0fdfb96cc198ae6af0ce8b350bf693ef.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -30,7 +30,6 @@
 #include "ufshci.h"
 #include "ufs-qcom-debugfs.h"
 #include "ufs_quirks.h"
-#include "ufshcd-crypto-qti.h"
 
 #define MAX_PROP_SIZE		   32
 #define VDDP_REF_CLK_MIN_UV        1200000
@@ -2103,12 +2102,6 @@ static int ufs_qcom_init(struct ufs_hba *hba)
 
 	/* restore the secure configuration */
 	ufs_qcom_update_sec_cfg(hba, true);
-
-	/*
-	 * Set the vendor specific ops needed for ICE.
-	 * Default implementation if the ops are not set.
-	 */
-	ufshcd_crypto_qti_set_vops(hba);
 
 	err = ufs_qcom_bus_register(host);
 	if (err)
