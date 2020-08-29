bdb23afe7f68e9da8585df42f7c9847edd5026d8
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-29 10:30:33 +0530

    Revert "ANDROID: fscrypt: handle direct I/O with IV_INO_LBLK_32"
    This reverts commit 8700f864c231d92937be95bf19c56b012dfc7f32.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3855,9 +3855,12 @@ static ssize_t ext4_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	ssize_t ret;
 	int rw = iov_iter_rw(iter);
 
-	if (!fscrypt_dio_supported(iocb, iter))
-		return 0;
-
+	if (IS_ENABLED(CONFIG_FS_ENCRYPTION) && IS_ENCRYPTED(inode)) {
+		if (!fscrypt_inode_uses_inline_crypto(inode) ||
+		    !IS_ALIGNED(iocb->ki_pos | iov_iter_alignment(iter),
+				i_blocksize(inode)))
+			return 0;
+	}
 	if (fsverity_active(inode))
 		return 0;
 
