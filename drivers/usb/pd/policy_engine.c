a7e375e1bea3aee6ff2fead11e82b27739761f7a
Rohith Kollalsi <rkollals@codeaurora.org>
2020-11-09 23:18:40 +0530

    usb: dwc3: Ensure blocking_sync waits until host mode starts or stops
    Currently flushwork and drain_workqueue of resume work and sm_work
    queue is taking care of blocking until host mode starts or stops.
    Flush work only flushes the work which is currently being executed,
    it cannot be guaranteed that entire dwc3_wq will be flushed by
    flush_work, so when sm_work_queue is being drained there is still
    a chance that resume_work queues work to sm_work_queue which is not
    allowed and leads to dr swap failure.
    
    So based on the event from policy engine if it is start host or
    stop host ensure that blocking sync blocks until the event is
    completely executed.
    
    Change-Id: Ice5f5e8e2a39fc444cdec63475a737a44be48619
    Signed-off-by: Rohith Kollalsi <rkollals@codeaurora.org>

--- drivers/usb/pd/policy_engine.c
+++ drivers/usb/pd/policy_engine.c
@@ -354,6 +354,10 @@ static void *usbpd_ipc_log;
 #define ID_HDR_PRODUCT_AMA	5
 #define ID_HDR_PRODUCT_VPD	6
 
+/* params for usb_blocking_sync */
+#define STOP_USB_HOST		0
+#define START_USB_HOST		1
+
 static bool check_vsafe0v = true;
 module_param(check_vsafe0v, bool, 0600);
 
@@ -615,7 +619,7 @@ static int usbpd_release_ss_lane(struct usbpd *pd,
 	stop_usb_host(pd);
 
 	/* blocks until USB host is completely stopped */
-	ret = extcon_blocking_sync(pd->extcon, EXTCON_USB_HOST, 0);
+	ret = extcon_blocking_sync(pd->extcon, EXTCON_USB_HOST, STOP_USB_HOST);
 	if (ret) {
 		usbpd_err(&pd->dev, "err(%d) stopping host", ret);
 		goto err_exit;
@@ -2343,7 +2347,8 @@ static void dr_swap(struct usbpd *pd)
 			start_usb_host(pd, true);
 
 		/* ensure host is started before allowing DP */
-		extcon_blocking_sync(pd->extcon, EXTCON_USB_HOST, 0);
+		extcon_blocking_sync(pd->extcon, EXTCON_USB_HOST,
+					START_USB_HOST);
 
 		usbpd_send_svdm(pd, USBPD_SID, USBPD_SVDM_DISCOVER_IDENTITY,
 				SVDM_CMD_TYPE_INITIATOR, 0, NULL, 0);
