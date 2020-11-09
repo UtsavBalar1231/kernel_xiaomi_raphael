8eb25f76b93684bb38ad01b9d4bb36bf0d00655b
Subramanian Ananthanarayanan <skananth@codeaurora.org>
2020-11-09 13:03:00 +0530

    msm: ep_pcie: Changes to avoid handle_perst_wq schedule out
    Queue handle_perst_wq as system_highpri_wq to avoid WQ getting
    scheduled out.
    
    Change-Id: I28fdc6e3d51b98b132c04bf552c40c9dad3cbd0f
    Signed-off-by: Subramanian Ananthanarayanan <skananth@codeaurora.org>

--- drivers/platform/msm/ep_pcie/ep_pcie_core.c
+++ drivers/platform/msm/ep_pcie/ep_pcie_core.c
@@ -2360,7 +2360,7 @@ static irqreturn_t ep_pcie_handle_perst_irq(int irq, void *data)
 		atomic_set(&dev->perst_deast, perst ? 1 : 0);
 		if (perst) {
 			/* start work for link enumeration with the host side */
-			schedule_work(&dev->handle_perst_work);
+			queue_work(system_highpri_wq, &dev->handle_perst_work);
 		} else {
 			dev->no_notify = true;
 			/* shutdown the link if the link is already on */
