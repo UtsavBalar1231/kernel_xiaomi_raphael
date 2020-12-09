1c877fe1cf8a9a2f26b88667cb4a196aa06827f7
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-12-09 09:49:46 +0530

    Revert "sched/fair: Avoid cpu idle to deep sleep when have active balance"
    This reverts commit 07e7e2c2cc32c7d1699e85bc6a5af5f2e4d9b9a6.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11141,7 +11141,6 @@ no_move:
 				busiest->active_balance = 1;
 				busiest->push_cpu = this_cpu;
 				active_balance = 1;
-				mark_reserved(this_cpu);
 			}
 			raw_spin_unlock_irqrestore(&busiest->lock, flags);
 
@@ -11536,7 +11535,6 @@ out_unlock:
 	busiest_rq->active_balance = 0;
 	push_task = busiest_rq->push_task;
 	target_cpu = busiest_rq->push_cpu;
-	clear_reserved(target_cpu);
 
 	if (push_task)
 		busiest_rq->push_task = NULL;
@@ -11547,6 +11545,7 @@ out_unlock:
 		if (push_task_detached)
 			attach_one_task(target_rq, push_task);
 		put_task_struct(push_task);
+		clear_reserved(target_cpu);
 	}
 
 	if (p)
