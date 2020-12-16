c15e7b3d0410895042cc9165f4ee45cef4af0c02
Mayank Rana <mrana@codeaurora.org>
2020-12-16 09:41:49 -0800

    coresight: Don't update year with Linaro copyright
    commit b5e2142cea10 ("coresight: etr: Add support to use DDR based
    memory for BAM2BAM mode") modified Linaro copyright year by mistake.
    Hence revert back original Linaro copyright year as it is not
    supposed to be modified.
    
    Change-Id: I7024e43bcb8e3bce7a4ac78426b8ca3c66be4c93
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-tmc.h
+++ drivers/hwtracing/coresight/coresight-tmc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2015, 2020 Linaro Limited. All rights reserved.
+ * Copyright(C) 2015 Linaro Limited. All rights reserved.
  * Author: Mathieu Poirier <mathieu.poirier@linaro.org>
  *
  * This program is free software; you can redistribute it and/or modify it
