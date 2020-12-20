33c43229851533665d96bbe93d6025d39830da71
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-19 19:38:29 -0800

    msm: kgsl: Affine IRQ and worker kthread to the big CPU cluster
    These are in the critical path for rendering frames to the display, so
    mark them as performance-critical and affine them to the big CPU
    cluster. They aren't placed onto the prime cluster because the
    single-CPU prime cluster will be used to run the DRM IRQ and kthreads.
    DRM is more latency-critical than KGSL and we need to have DRM and KGSL
    running on separate CPUs for the best performance, so KGSL gets the big
    cluster.
    
    Change-Id: Ia2392a5ed83b6d08a967a03000458f3092bb2753
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5014,8 +5014,8 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	}
 
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				  kgsl_irq_handler, IRQF_TRIGGER_HIGH,
-				  device->name, device);
+				 kgsl_irq_handler, IRQF_TRIGGER_HIGH |
+				 IRQF_PERF_AFFINE, device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
 			      device->pwrctrl.interrupt_num, status);
@@ -5244,8 +5244,8 @@ static int __init kgsl_core_init(void)
 
 	kthread_init_worker(&kgsl_driver.worker);
 
-	kgsl_driver.worker_thread = kthread_run(kthread_worker_fn,
-		&kgsl_driver.worker, "kgsl_worker_thread");
+	kgsl_driver.worker_thread = kthread_run_perf_critical(cpu_perf_mask,
+		kthread_worker_fn, &kgsl_driver.worker, "kgsl_worker_thread");
 
 	if (IS_ERR(kgsl_driver.worker_thread)) {
 		pr_err("unable to start kgsl thread\n");
