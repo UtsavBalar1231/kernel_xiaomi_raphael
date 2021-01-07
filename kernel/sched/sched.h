8db43faa925fd7c68b45494b5b01dde9bc077234
Diep Quynh <remilia.1505@gmail.com>
2021-01-07 14:46:29 +0700

    sched: fair: Fix load balancing for big tasks
    cpu_capacity() returns maximum capacity when WALT is disabled, hence
    we couldn't take advantage of CAF's optimization
    Return CPU's original capacity instead to make it usable
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3035,7 +3035,7 @@ static inline bool is_min_capacity_cpu(int cpu)
 #ifdef CONFIG_SMP
 static inline int cpu_capacity(int cpu)
 {
-	return SCHED_CAPACITY_SCALE;
+	return capacity_orig_of(cpu);
 }
 #endif
 
