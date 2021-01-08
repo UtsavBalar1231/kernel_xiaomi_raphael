c42e74171da8ad94dc616d6c60e7c2f48aac95fe
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-08 21:49:30 +0530

    Revert "sched/fair: fix merge conflict"
    This reverts commit af704a0c9120c5b2c54f8ab52763da2abbfb3b0c.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5808,7 +5808,7 @@ cpu_is_in_target_set(struct task_struct *p, int cpu)
 	struct root_domain *rd = cpu_rq(cpu)->rd;
 	int first_cpu, next_usable_cpu;
 
-	if (schedtune_prefer_high_cap(p)) {
+	if (schedtune_task_boost(p)) {
 		first_cpu = rd->mid_cap_orig_cpu != -1 ? rd->mid_cap_orig_cpu :
 			    rd->max_cap_orig_cpu;
 
