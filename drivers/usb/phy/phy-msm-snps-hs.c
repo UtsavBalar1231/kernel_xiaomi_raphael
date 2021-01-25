9cf9266650f34594d567ecc994e9c82a809f8525
Ajay Agarwal <ajaya@codeaurora.org>
2021-01-25 18:48:47 +0530

    usb: phy: snps: Enable autoresume for FS/HS peripherals only
    Currently the driver enables autoresume for all the connected
    peripherals. In a case where an LS device is connected but fails
    the enumeration, this autoresume enablement is leading to PHY
    keeping the Dm pulled up even when the peripheral is detached.
    Hence, the detach hs_phy_irq is never fired and disconnect is
    not processed.
    Since remote wakeup from LS peripherals is not a requirement on
    our platforms, enable autoresume for FS/HS devices only.
    
    Change-Id: I0a62d08e58af18b523af322c64529f5183d55907
    Signed-off-by: Ajay Agarwal <ajaya@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/usb/phy/phy-msm-snps-hs.c
+++ drivers/usb/phy/phy-msm-snps-hs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -516,12 +516,11 @@ static int msm_hsphy_set_suspend(struct usb_phy *uphy, int suspend)
 
 	if (suspend) { /* Bus suspend */
 		if (phy->cable_connected) {
-			/* Enable auto-resume functionality only during host
-			 * mode bus suspend with some peripheral connected.
+			/* Enable auto-resume functionality during host mode
+			 * bus suspend with some FS/HS peripheral connected.
 			 */
 			if ((phy->phy.flags & PHY_HOST_MODE) &&
-				((phy->phy.flags & PHY_HSFS_MODE) ||
-				(phy->phy.flags & PHY_LS_MODE))) {
+				(phy->phy.flags & PHY_HSFS_MODE)) {
 				/* Enable auto-resume functionality by pulsing
 				 * signal
 				 */
