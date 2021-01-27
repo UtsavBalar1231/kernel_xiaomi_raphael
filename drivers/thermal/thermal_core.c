c30373fc3313d9ac58704adbeb64bb7f52030b4d
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-27 15:32:32 +0000

    Revert "thermal: core: Workaround for xiaomi thermal mitigation"
    This reverts commit d6db441bc4e6c94affebf6ef66de79f5823b139c.

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -65,7 +65,7 @@ struct screen_monitor {
 struct screen_monitor sm;
 #endif
 
-static atomic_t switch_mode = ATOMIC_INIT(10);
+static atomic_t switch_mode = ATOMIC_INIT(-1);
 static atomic_t temp_state = ATOMIC_INIT(0);
 static char boost_buf[128];
 
@@ -1666,14 +1666,12 @@ static ssize_t
 thermal_sconfig_store(struct device *dev,
 struct device_attribute *attr, const char *buf, size_t len)
 {
-	int ret, val = -1;
+	int val = -1;
 
-	ret = kstrtoint(buf, 10, &val);
+	val = simple_strtol(buf, NULL, 10);
 
 	atomic_set(&switch_mode, val);
 
-	if (ret)
-		return ret;
 	return len;
 }
 
@@ -1711,14 +1709,12 @@ static ssize_t
 thermal_temp_state_store(struct device *dev,
 struct device_attribute *attr, const char *buf, size_t len)
 {
-	int ret, val = -1;
+	int val = -1;
 
-	ret = kstrtoint(buf, 10, &val);
+	val = simple_strtol(buf, NULL, 10);
 
 	atomic_set(&temp_state, val);
 
-	if (ret)
-		return ret;
 	return len;
 }
 
