e93597082429ee1c02e61880d5f5bf59fb0e1f9f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-03-23 16:24:02 +0000

    Revert "mm: introduce NR_UNRECLAIMABLE_PAGES"
    This reverts commit 1f1ecb4423da3ef7072f9a6d3b6ee79384458c3b.

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1095,7 +1095,6 @@ const char * const vmstat_text[] = {
 	"nr_dirtied",
 	"nr_written",
 	"nr_indirectly_reclaimable",
-	"nr_unreclaimable_pages",
 
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
