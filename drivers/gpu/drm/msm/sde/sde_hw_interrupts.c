cb4c7a4868db94b4bb6f826ee65f9b9f242c0209
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-06 14:59:13 +0200

    Revert "drm/msm/sde: Fix format specifier warnings"
    This reverts commit c5e9b934c9a67e759257b77235bd96de40d9e634.

--- drivers/gpu/drm/msm/sde/sde_hw_interrupts.c
+++ drivers/gpu/drm/msm/sde/sde_hw_interrupts.c
@@ -1521,7 +1521,7 @@ struct sde_hw_intr *sde_hw_intr_init(void __iomem *addr,
 	}
 
 	if (count <= 0 || count > MDSS_INTR_MAX) {
-		pr_err("wrong mapping of supported irqs 0x%lx\n",
+		pr_err("wrong mapping of supported irqs 0x%x\n",
 			m->mdss_irqs[0]);
 		ret = -EINVAL;
 		goto exit;
